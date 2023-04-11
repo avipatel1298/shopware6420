24f69272d917ac7bab4759b58f917befba238a6b
avipatel1298 <avi.patel@icreativetecnolabs.com>
2023-04-11 18:32:20 +0530

    11-04-2023

--- custom/plugins/SwagFirstPluginExample/src/Core/Content/FirstPlugin/FirstPluginDefinition.php
+++ custom/plugins/SwagFirstPluginExample/src/Core/Content/FirstPlugin/FirstPluginDefinition.php
@@ -11,8 +11,12 @@ use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\ManyToOneAssociationField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\ReferenceVersionField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Inherited;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\TranslatedField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\TranslationsAssociationField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
+use SwagFirstPluginExample\Core\Content\FirstPlugin\Aggregate\FirstPluginTranslation\FirstPluginTranslationDefinition;
 
 
 class FirstPluginDefinition extends EntityDefinition
@@ -39,15 +43,19 @@ class FirstPluginDefinition extends EntityDefinition
     {
         return new FieldCollection([
             (new IdField('id', 'id'))->addFlags(new Required(), new PrimaryKey()),
-            (new StringField('name', 'name'))->addFlags(new Required()),
+            (new StringField('not_translated_field', 'notTranslatedField'))->addFlags(new ApiAware()),
+            (new TranslatedField('name')),
             (new StringField('email', 'email')),
             (new StringField('password', 'password')),
-            (new FkField('product_id', 'productId', ProductDefinition::class)),
-            (new StringField('product_name', 'product_name')),
-            (new ReferenceVersionField(ProductDefinition::class))->addFlags(new ApiAware()),
+            (new FkField('product_id', 'productId', ProductDefinition::class))->addFlags(new ApiAware(), new Required()),
+            (new StringField('product_name', 'productName')),
+            (new ReferenceVersionField(ProductDefinition::class))->addFlags(new ApiAware(), new Inherited()),
             (new StringField('product_number', 'product_number')),
+            (new TranslationsAssociationField(
+                FirstPluginTranslationDefinition::class,
+                'name', 'name'))->addFlags(new ApiAware(), new Required()),
 
-            new ManyToOneAssociationField('product', 'product_id', ProductDefinition::class, 'id'),
+            new ManyToOneAssociationField('productId', 'product_id', ProductDefinition::class, 'id'),
 
         ]);
     }
