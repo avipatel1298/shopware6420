a2e1842ec6e8b912543bace7bacded5c1872d53e
avipatel1298 <avi.patel@icreativetecnolabs.com>
2023-05-08 19:02:14 +0530

    08-05-2023 Created review slider and add data to it and showed on strofront

--- custom/plugins/ICTECHQuickViewPopupWithPastPurchasedProduct/src/Core/Content/Product/Cms/IctechProductsReviewSliderCmsElementResolver.php
+++ custom/plugins/ICTECHQuickViewPopupWithPastPurchasedProduct/src/Core/Content/Product/Cms/IctechProductsReviewSliderCmsElementResolver.php
@@ -2,12 +2,16 @@
 
 namespace ICTECHQuickViewPopupWithPastPurchasedProduct\Core\Content\Product\Cms;
 
+
 use Shopware\Core\Content\Cms\Aggregate\CmsSlot\CmsSlotEntity;
 use Shopware\Core\Content\Cms\DataResolver\CriteriaCollection;
 use Shopware\Core\Content\Cms\DataResolver\Element\AbstractCmsElementResolver;
 use Shopware\Core\Content\Cms\DataResolver\Element\ElementDataCollection;
 use Shopware\Core\Content\Cms\DataResolver\ResolverContext\ResolverContext;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Log\Package;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 #[Package('inventory')]
@@ -16,13 +20,17 @@ class IctechProductsReviewSliderCmsElementResolver extends AbstractCmsElementRes
 
     private SystemConfigService $systemConfigService;
 
+    private EntityRepositoryInterface $productReviewRepository;
+
+
     /**
      * @internal
      */
-    public function __construct( SystemConfigService $systemConfigService)
+    public function __construct(SystemConfigService $systemConfigService, EntityRepositoryInterface $productReviewRepository)
     {
 
         $this->systemConfigService = $systemConfigService;
+        $this->productReviewRepository = $productReviewRepository;
     }
 
     public function getType(): string
@@ -36,15 +44,28 @@ class IctechProductsReviewSliderCmsElementResolver extends AbstractCmsElementRes
         $collection = new CriteriaCollection();
 
 
-
         return $collection->all() ? $collection : null;
     }
 
     public function enrich(CmsSlotEntity $slot, ResolverContext $resolverContext, ElementDataCollection $result): void
     {
         $config = $slot->getFieldConfig();
+        $slot->setFieldConfig($config);
+        $context = $resolverContext->getSalesChannelContext()->getContext();
+        $productId = $resolverContext->getRequest()->attributes->get('productId');
+        $criteria = new Criteria();
 
-//        $slot->setData($slider);
+        if ($productId !== null) {
+            $criteria->addFilter((new EqualsFilter('productId', $productId)));
+            $criteria->addAssociation('customer');
+            $reviews = $this->productReviewRepository->search($criteria, $context);
+//
+//            dd($reviews);
+            $slot->setData($reviews);
+        }
+//        dd($slot);
+
+//        dd($productId);
 
 
     }
