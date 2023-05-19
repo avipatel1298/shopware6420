ca0493e7f0488189497a1b9d6de371b15cd873b6
avipatel1298 <avi.patel@icreativetecnolabs.com>
2023-05-19 19:19:19 +0530

    may-19-2023

--- custom/plugins/ICTECHQuickViewPopupWithPastPurchasedProduct/src/Storefront/Controller/QuickViewPopUpController.php
+++ custom/plugins/ICTECHQuickViewPopupWithPastPurchasedProduct/src/Storefront/Controller/QuickViewPopUpController.php
@@ -1,30 +1,53 @@
-<?php declare(strict_types=1);
-
-namespace ICTECHQuickViewPopupWithPastPurchasedProduct\Storefront\Controller;
-
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
-use Shopware\Storefront\Controller\StorefrontController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-
-/**
- * @Route(defaults={"_routeScope"={"storefront"}})
- */
-class QuickViewPopUpController extends StorefrontController
-{
-    private EntityRepository $productRepository;
-
-    public function __construct(EntityRepository $productRepository)
-    {
-        $this->productRepository = $productRepository;
-    }
-
-    /**
-     * @Route("/product/product-detail/{productId}", name="frontend.product.detail.page", methods={"GET"}, defaults={"productId"=null, "XmlHttpRequest"=true})
-     */
-    public function index(Request $request, string $productId): Response
-    {
-        return $page = $this->renderStorefront('@ICTECHQuickViewPopupWithPastPurchasedProduct/storefront/page/product-detail/product-detail-index.html.twig');
-    }
-}
+<?php
+//
+//namespace ICTECHQuickViewPopupWithPastPurchasedProduct\Storefront\Controller;
+//
+//use Shopware\Core\Content\Product\ProductEntity;
+//use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+//use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+//use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+//use Shopware\Core\System\SalesChannel\SalesChannelContext;
+//use Shopware\Storefront\Framework\Cache\Annotation\HttpCache;
+//use Shopware\Storefront\Page\Product\ProductPageLoader;
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\HttpFoundation\Response;
+//
+//
+///**
+// * @RouteScope(scopes={"storefront"})
+// */
+//class QuickViewPopUpController extends AbstractController
+//{
+//    /**
+//     * @var ProductPageLoader
+//     */
+//    private $productPageLoader;
+//
+//    private $productRepository;
+//
+//    public function __construct(
+//
+//        ProductPageLoader         $productPageLoader,
+//        EntityRepositoryInterface $productRepository
+//    )
+//    {
+//        $this->productPageLoader = $productPageLoader;
+//        $this->productRepository = $productRepository;
+//    }
+//
+//    /**
+//     * @HttpCache()
+//     * @Route("/detail/{productId}", name="frontend.product.detail.page", methods={"GET"}, defaults={"XmlHttpRequest": true})
+//     */
+//    public function detail(string $productId, Request $request, SalesChannelContext $context): Response
+//    {
+//        $product = $this->productRepository->search(new Criteria([$productId]), $context->getContext())->first();
+//
+//        // Render the product detail page template with the loaded page data
+//        return $this->renderStorefront('@ICTECHQuickViewPopupWithPastPurchasedProduct/storefront/page/product-detail/index.html.twig', [
+//            'product' => $product,
+//        ]);
+//    }
+//}
